977411b8cbf2050d710aba19f978984f249e5ef8
Анна Никулина <anna.nikulina89@mail.ru>
2022-05-04 21:30:32 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,14 @@
 public class Main {
-  public static void main(String[] args) {
-    BonusService service = new BonusService();
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
 
-    long amount = 1000_60;
-    boolean registered = true;
+        long amount = 1000_60;
+        boolean registered = true;
 
-    service.calculate(amount, registered);
-  }
+        long actual = service.calculate(amount, registered);
+        int expected = 30;
+
+        System.out.println(actual == expected);
+    }
 }
 
